381dbf0a3dac2ccc0da1cda2cdfb6016811bd8a6
Dávid Pólus/school <polus.david.gyula@gmail.com>
2018-07-16 10:15:12 +0200

    New parts add to Markoperatot form.

--- AEN/Markoperator.Designer.cs
+++ AEN/Markoperator.Designer.cs
@@ -41,6 +41,10 @@
             this.activeCheckBox = new System.Windows.Forms.CheckBox();
             this.classComboBox = new System.Windows.Forms.ComboBox();
             this.studenNameCombobox = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.logOutButton)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.minimizeButton)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.exitButton)).BeginInit();
@@ -98,6 +102,10 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.endDateTimePicker);
             this.panel2.Controls.Add(this.startDateTimePicker);
             this.panel2.Controls.Add(this.label1);
@@ -105,39 +113,40 @@
             this.panel2.Controls.Add(this.activeCheckBox);
             this.panel2.Controls.Add(this.classComboBox);
             this.panel2.Controls.Add(this.studenNameCombobox);
-            this.panel2.Location = new System.Drawing.Point(39, 50);
+            this.panel2.Location = new System.Drawing.Point(12, 50);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(230, 286);
+            this.panel2.Size = new System.Drawing.Size(257, 286);
             this.panel2.TabIndex = 30;
             // 
             // endDateTimePicker
             // 
-            this.endDateTimePicker.Location = new System.Drawing.Point(85, 248);
+            this.endDateTimePicker.Location = new System.Drawing.Point(99, 239);
             this.endDateTimePicker.Name = "endDateTimePicker";
             this.endDateTimePicker.Size = new System.Drawing.Size(130, 20);
             this.endDateTimePicker.TabIndex = 6;
             // 
             // startDateTimePicker
             // 
-            this.startDateTimePicker.Location = new System.Drawing.Point(82, 207);
+            this.startDateTimePicker.Location = new System.Drawing.Point(99, 198);
             this.startDateTimePicker.Name = "startDateTimePicker";
-            this.startDateTimePicker.Size = new System.Drawing.Size(133, 20);
+            this.startDateTimePicker.Size = new System.Drawing.Size(130, 20);
             this.startDateTimePicker.TabIndex = 5;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(82, 38);
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(96, 29);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.Size = new System.Drawing.Size(52, 20);
             this.label1.TabIndex = 4;
             this.label1.Text = "Tanuló:";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(17, 163);
+            this.comboBox1.Location = new System.Drawing.Point(31, 154);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(196, 21);
             this.comboBox1.TabIndex = 3;
@@ -145,7 +154,8 @@
             // activeCheckBox
             // 
             this.activeCheckBox.AutoSize = true;
-            this.activeCheckBox.Location = new System.Drawing.Point(135, 110);
+            this.activeCheckBox.ForeColor = System.Drawing.SystemColors.Control;
+            this.activeCheckBox.Location = new System.Drawing.Point(151, 103);
             this.activeCheckBox.Name = "activeCheckBox";
             this.activeCheckBox.Size = new System.Drawing.Size(78, 17);
             this.activeCheckBox.TabIndex = 2;
@@ -155,7 +165,7 @@
             // classComboBox
             // 
             this.classComboBox.FormattingEnabled = true;
-            this.classComboBox.Location = new System.Drawing.Point(17, 108);
+            this.classComboBox.Location = new System.Drawing.Point(31, 99);
             this.classComboBox.Name = "classComboBox";
             this.classComboBox.Size = new System.Drawing.Size(44, 21);
             this.classComboBox.TabIndex = 1;
@@ -163,10 +173,54 @@
             // studenNameCombobox
             // 
             this.studenNameCombobox.FormattingEnabled = true;
-            this.studenNameCombobox.Location = new System.Drawing.Point(17, 61);
+            this.studenNameCombobox.Location = new System.Drawing.Point(31, 52);
             this.studenNameCombobox.Name = "studenNameCombobox";
             this.studenNameCombobox.Size = new System.Drawing.Size(198, 21);
             this.studenNameCombobox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(31, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Osztály:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(31, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ettől:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.ForeColor = System.Drawing.SystemColors.Control;
+            this.label4.Location = new System.Drawing.Point(31, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Eddig:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.ForeColor = System.Drawing.SystemColors.Control;
+            this.label5.Location = new System.Drawing.Point(100, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Tantárgy:";
             // 
             // Markoperator
             // 
@@ -208,5 +262,9 @@
         private System.Windows.Forms.CheckBox activeCheckBox;
         private System.Windows.Forms.ComboBox classComboBox;
         public System.Windows.Forms.ComboBox studenNameCombobox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
     }
 }
